18bd9b1f0dc528daeb9beb132c5420395cb7a8f8
Xianliang He <hexianliang9@gmail.com>
2020-08-18 17:51:22 -0400

    saving changes

--- src/redux/Cars/cars.actions.js
+++ src/redux/Cars/cars.actions.js
@@ -7,3 +7,9 @@ export const updateCar = car => ({
     type: 'UPDATE_CAR',
     payload: car
 });
+
+export const fetchCars = car => ({ type: 'FETCH_POSTS' });
+
+export const fetchCarsSuccess = car => ({ type: 'FETCH_POSTS', status: 'error', error: 'Oops' });
+
+export const fetchCarsFail = car => ({ type: 'FETCH_POSTS', status: 'success', response: {} });
